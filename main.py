16ac71da30928d639c8f527dff45e582b19a118a
vicrrs <enricofermi2019@gmail.com>
2025-01-10 02:55:14 -0400

    Preparar a Pergunta e Processar a Entrada

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from transformers import BertTokenizer, BertModel
 import torch
 
 # Carregar o tockenizer e o modelo BERT pré-treinado
-tockenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
 
 # Configurando o modelo pra avaliação
 model.eval()
+
+# Definir a pergunra
+pergunta = "Quais são as 3 Leis de Newton?"
+
+# Tokenizar a entrada
+inputs = tokenizer(pergunta, return_tensors="pt")
+
+# Passar a entrada pelo modelo
+with torch.no_grad():
+    outputs = model(**inputs)
+
+# Extraindo as atenções
+attentions = outputs.attentions
